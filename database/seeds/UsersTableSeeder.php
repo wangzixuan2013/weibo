c0a0831a3dfaf5ee6e6753bf3622eceee1fcc8f7
wangzixuan2013 <wanghuanle113@126.com>
2019-11-21 07:21:26 +0000

    查看用户列表

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $users = factory(\App\Models\User::class)->times(50)->make();
+        \App\Models\User::insert($users->makeVisible(['password', 'remember_token'])->toArray());
+        $user = \App\Models\User::find(1);
+        $user->name = 'gavin';
+        $user->email = 'wanghuanle113@126.com';
+        $user->save();
+    }
+}
