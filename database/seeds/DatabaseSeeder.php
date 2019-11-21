2036651b31dea5e34ea62bc753c5481f7f678b67
wangzixuan2013 <wanghuanle113@126.com>
2019-11-21 10:54:56 +0000

    用户微博列表

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
         \Illuminate\Database\Eloquent\Model::unguard();
 
         $this->call(UsersTableSeeder::class);
+        $this->call(StatusesTableSeeder::class);
 
         \Illuminate\Database\Eloquent\Model::reguard();
     }
