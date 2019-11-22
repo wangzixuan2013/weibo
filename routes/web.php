f32c5e93b7cc28549656d615a81a55f44fac2c3a
wangzixuan2013 <wanghuanle113@126.com>
2019-11-22 11:32:31 +0000

    关注和粉丝列表页面

--- routes/web.php
+++ routes/web.php
@@ -44,3 +44,7 @@ Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('passw
 
 /*微博相关*/
 Route::resource('statuses', 'StatusesController', ['only' => ['store', 'destroy']]);
+
+/*粉丝关注*/
+Route::get('/users/{user}/followings', 'UsersController@followings')->name('users.followings');
+Route::get('/users/{user}/followers', 'UsersController@followers')->name('users.followers');
