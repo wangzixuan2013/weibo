318df87257165206ed0c0c97a2f50f729dd6fbff
wangzixuan2013 <wanghuanle113@126.com>
2019-11-22 11:27:53 +0000

    社交统计信息

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,11 +1,16 @@
 @extends('layouts.default')
 @section('title', $user->name)
+
 @section('content')
     <div class="row">
         <div class="offset-md-2 col-md-8">
             <section class="user_info">
                 @include('shared._user_info', ['user' => $user])
             </section>
+            <section class="stats mt-2">
+                @include('shared._stats', ['user' => $user])
+            </section>
+            <hr>
             <section class="status">
                 @if ($statuses->count() > 0)
                     <ul class="list-unstyled">
