abd047ed21fbe7fa124c8fe45be6855ba9483dd9
wangzixuan2013 <wanghuanle113@126.com>
2019-11-22 11:45:23 +0000

    关注按钮

--- routes/web.php
+++ routes/web.php
@@ -48,3 +48,5 @@ Route::resource('statuses', 'StatusesController', ['only' => ['store', 'destroy'
 /*粉丝关注*/
 Route::get('/users/{user}/followings', 'UsersController@followings')->name('users.followings');
 Route::get('/users/{user}/followers', 'UsersController@followers')->name('users.followers');
+Route::post('/users/followers/{user}', 'FollowersController@store')->name('followers.store');
+Route::delete('/users/followers/{user}', 'FollowersController@destroy')->name('followers.destroy');
